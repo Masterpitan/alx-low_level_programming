2186a4f8484ca08c56d4bf95c6367e1f681725d6
Masterpitan <akoredeadetunji93@gmail.com>
2023-02-19 01:40:49 -0800

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - A program prints all possible different combinations
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a = '0';
+	int b = 'b';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			if (!(a > b || a == b))
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					pputchar('');
+				}
+			}
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	return (0);
+}
