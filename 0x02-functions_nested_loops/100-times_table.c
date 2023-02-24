78ff6b5452d43305ed17203af123daf0b2cd8bd7
Masterpitan <akoredeadetunji93@gmail.com>
2023-02-23 16:51:31 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,11 +6,12 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <=; j++)
+			for (j = 0; j <= n; j++)
 			{
 				k = J * i;
 				if (j == 0)
@@ -30,7 +31,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((k / 10) + '0);
+					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
