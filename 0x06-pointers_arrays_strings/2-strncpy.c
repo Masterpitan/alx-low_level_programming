f641ce1d1244b3d58bed47e7b2c7b93b21bb7e3a
Masterpitan <akoredeadetunji93@gmail.com>
2023-03-01 10:53:11 -0800

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates n bytes from one string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to contatenate
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (strc[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
