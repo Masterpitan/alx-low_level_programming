dbbcd2ffcd5f05c01076a9f30a441da868d450f5
Masterpitan <akoredeadetunji93@gmail.com>
2023-03-06 14:03:49 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (i = 0; needle[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
 				break;
