dd77da1ef19c1f6df340c1bdff8b612be6f220ce
Masterpitan <akoredeadetunji93@gmail.com>
2023-03-08 09:41:34 -0800

    100-is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen_recursion - function that prints size
- * @s:pointer to string palindrome
+ * @s:pointer to string params
  * Return: recursion
  */
 int _strlen_recursion(char *s)
@@ -18,4 +18,28 @@ int _strlen_recursion(char *s)
  * @s: pointer to string
  * @l: position
  * Return: boolena
- * 
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + 1))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
+}
