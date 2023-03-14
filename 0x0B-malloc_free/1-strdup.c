465215c47fa6c4e84b159e5b79701325daad0bf0
Masterpitan <akoredeadetunji93@gmail.com>
2023-03-14 03:37:53 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function that prints the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
