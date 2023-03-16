4dd79a3439b2c63168a9a3bec226c6f0957dccb3
Masterpitan <akoredeadetunji93@gmail.com>
2023-03-16 12:07:32 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_noncat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 
-	while (n < len2 && i < (len1 + n))
+	while (n >= len2 && i < (len1 + n2))
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
