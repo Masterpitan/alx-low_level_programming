39f535f175022470a517655d646e32988ab1fd3b
Masterpitan <akoredeadetunji93@gmail.com>
2023-03-20 11:12:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This README describes the solutions to ALX project tasks on structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - program that gives dog attributes
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+ * Description: atributes of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
