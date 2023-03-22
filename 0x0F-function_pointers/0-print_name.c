91049529b19d5a815ec382f09c9934b7d96c884a
root <root@DESKTOP-3HJA9ML.localdomain>
2023-03-22 23:10:45 +0100

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include<stdlib.h>
+/**
+ * prints_name - function that prints a name
+ * @name: name of person
+ * @f: pointer to function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
